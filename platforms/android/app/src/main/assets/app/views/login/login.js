ac94155b95ca979ddb201794bc3af762b96a1e55
John Fortune <wizmanjohn>
2018-02-12 15:36:02 -0400

    Grocery App

---
+++ platforms/android/app/src/main/assets/app/views/login/login.js
@@ -0,0 +1,36 @@
+var socialShare = require("nativescript-social-share");
+var UserViewModel = require("../../shared/view-models/user-view-model");
+var user = new UserViewModel();
+var dialogsModule = require("ui/dialogs");
+var frameModule = require("ui/frame");
+var page;
+var email;
+
+exports.loaded = function(args) {
+    page = args.object;
+    if (page.ios) {
+        var navigationBar = frameModule.topmost().ios.controller.navigationBar;
+        navigationBar.barStyle = UIBarStyle.UIBarStyleBlack;
+    }
+    page.bindingContext = user;
+};
+
+exports.signIn = function() {
+    user.login()
+        .catch(function(error) {
+            console.log(error);
+            dialogsModule.alert({
+                message: "Unfortunately we could not find your account.",
+                okButtonText: "OK"
+            });
+            return Promise.reject();
+        })
+        .then(function() {
+            frameModule.topmost().navigate("views/list/list");
+        });
+};
+
+exports.register = function() {
+    var topmost = frameModule.topmost();
+    topmost.navigate("views/register/register");
+};
